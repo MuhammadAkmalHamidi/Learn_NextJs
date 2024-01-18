bdf0cc655f678071c8e6a09bfb159825e98e801a
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2024-01-18 19:24:07 +0700

    styling with tailwind.css

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,7 +1,7 @@
 import styles from './styles.module.css'
 export default function Footer() {
     return (
-        <div className={styles.footer}>
+        <div className=" bg-zinc-700 h-32 flex items-center justify-center text-yellow-600 font-bold text-xl">
             Footer Components
         </div>
     )

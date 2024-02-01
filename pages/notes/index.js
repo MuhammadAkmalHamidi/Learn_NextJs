7fa0bc7d6c2d996cb2889705b95d67928c7dd925
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2024-02-01 23:41:47 +0700

    add custom hooks

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,21 +1,22 @@
 import EditNotesModal from "@/components/modal/edit-notes-modal";
+import { useQueries } from "@/hooks/useQueries";
 import Layout from "@/layout";
 import Axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function notes() {
-  const [data, setData] = useState([]);
+  const {data , isLoading, isError} = useQueries({prefixUrl: "https://paace-f178cafcae7b.nevacloud.io/api/notes"});
   const [reload, setReload] = useState(false);
-  const [open, setOpen] = useState(false)
-  const [id, setId] = useState()
+  const [open, setOpen] = useState(false);
+  const [id, setId] = useState();
 
-  const getData = async () => {
-    const response = await Axios.get(
-      "https://paace-f178cafcae7b.nevacloud.io/api/notes"
-    );
-    setData(response.data.data);
-  };
+  // const getData = async () => {
+  //   const response = await Axios.get(
+  //     "https://paace-f178cafcae7b.nevacloud.io/api/notes"
+  //   );
+  //   setData(response.data.data);
+  // };
 
   const deleteNotesById = async (id) => {
     try {
@@ -29,11 +30,17 @@ export default function notes() {
   };
 
   useEffect(() => {
-    getData();
+    // getData();
   }, [reload]);
   return (
     <>
-      <EditNotesModal open={open} setReload={setReload} reload={reload} setOpen={setOpen} id={id}/>
+      <EditNotesModal
+        open={open}
+        setReload={setReload}
+        reload={reload}
+        setOpen={setOpen}
+        id={id}
+      />
       <Layout>
         <div>
           <div className="flex px-10 pt-10  justify-end">
@@ -44,8 +51,8 @@ export default function notes() {
               Add Notes
             </Link>
           </div>
-          <div className=" flex flex-wrap gap-4">
-            {data?.map((item, index) => {
+          <div className="flex flex-wrap gap-4">
+            {data?.data.map((item, index) => {
               return (
                 <div className="w-1/4 p-5 border">
                   <Link
@@ -54,13 +61,16 @@ export default function notes() {
                     className=" cursor-pointer"
                   >
                     <p className=" mb-5 text-2xl font-bold ">{item.title}</p>
-                    <p>{item.description}</p>
+                    <p>{item.description.substring(0, 80) + "...."}</p>
                   </Link>
                   <div className=" h-1/3 flex">
-                    <button onClick={() => {
-                      setOpen(true)
-                      setId(item.id)
-                    }} className="w-1/2 flex font-bold justify-center items-center">
+                    <button
+                      onClick={() => {
+                        setOpen(true);
+                        setId(item.id);
+                      }}
+                      className="w-1/2 flex font-bold justify-center items-center"
+                    >
                       Edit
                     </button>
                     <button
